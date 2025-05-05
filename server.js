7db906de2bb46d2abc2c8e6cc21e1787d7b89817
oapalmer <ogeepalmerr@gmail.com>
2025-05-05 14:21:02 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
-
 app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('SoftAI Backend is working!');
+});
+
+app.post('/ask', (req, res) => {
+  const { question } = req.body;
+  const reply = `You asked: "${question}". Lily's brain is warming up!`;
+  res.json({ answer: reply });
 });
 
 app.listen(3000, () => {
